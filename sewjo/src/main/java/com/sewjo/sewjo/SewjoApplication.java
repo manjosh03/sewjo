54c363183e4ea1ac9450b9e5dc8e61a78470750f
manjosh03 <manjosh01@gmail.com>
2024-06-26 12:38:47 -0700

    Added initial files

---
+++ sewjo/src/main/java/com/sewjo/sewjo/SewjoApplication.java
@@ -0,0 +1,13 @@
+package com.sewjo.sewjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewjoApplication.class, args);
+	}
+
+}
---
+++ sewjo/src/test/java/com/sewjo/sewjo/SewjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sewjo.sewjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
