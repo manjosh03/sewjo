5b102a59a33563647f54e834aecdb186c36cfa27
Ali Naseri <ali@naseri.live>
2024-06-29 17:09:59 -0700

    Create Pattern Class and Interface

--- src/main/java/com/sewjo/sewjo/Models/Pattern.java
+++ src/main/java/com/sewjo/sewjo/Models/Pattern.java
@@ -1,4 +1,66 @@
 package com.sewjo.sewjo.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Patterns")
 public class Pattern {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String type;
+    private String description;
+    private String image;
+    private int projectId;
+
+    public Pattern() {
+    }
+
+    public Pattern(String name, String type, String description, String image) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
 }
