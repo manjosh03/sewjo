b5e4f8841c39aec570fafd038100633d66e648b9
Ali Naseri <ali@naseri.live>
2024-06-29 17:16:14 -0700

    Create the project class and methods

--- src/main/java/com/sewjo/sewjo/Models/Project.java
+++ src/main/java/com/sewjo/sewjo/Models/Project.java
@@ -1,4 +1,60 @@
 package com.sewjo.sewjo.Models;
+import java.util.List;
 
 public class Project {
+    private int id;
+    private String name;
+    private String description;
+    private String image;
+    private List<Integer> patternIds;
+    private List<Integer> fabricIds;
+
+    public Project() {
+    }
+
+    public Project(String name, String description, String image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<Integer> getPatternIds() {
+        return patternIds;
+    }
+
+    public void setPatternIds(List<Integer> patternIds) {
+        this.patternIds = patternIds;
+    }
+
+    public List<Integer> getFabricIds() {
+        return fabricIds;
+    }
+
+    public void setFabricIds(List<Integer> fabricIds) {
+        this.fabricIds = fabricIds;
+    }
 }
