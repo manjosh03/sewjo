18c11019c272ab2f14e990d4d99876e2d3c7c197
Ali Naseri <ali@naseri.live>
2024-06-29 17:09:59 -0700

    Create Pattern Class and Interface

--- src/main/java/com/sewjo/sewjo/Models/PatternRepo.java
+++ src/main/java/com/sewjo/sewjo/Models/PatternRepo.java
@@ -1,4 +1,12 @@
 package com.sewjo.sewjo.Models;
 
-public interface PatternRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PatternRepo extends JpaRepository<Pattern, Integer>{
+    List<Pattern> findAll();
+    List<Pattern> findByName(String name);
+    List<Pattern> findByProjectId(int projectId);
+    Pattern findById(int id);
+    void deleteById(int id);
 }
