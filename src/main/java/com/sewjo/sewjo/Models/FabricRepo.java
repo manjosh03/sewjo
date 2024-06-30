ce2293e67008c296a1c72e20f8f3eb5c510471fa
Ali Naseri <ali@naseri.live>
2024-06-29 17:03:11 -0700

    Create the fabric interface using JPA

--- src/main/java/com/sewjo/sewjo/Models/FabricRepo.java
+++ src/main/java/com/sewjo/sewjo/Models/FabricRepo.java
@@ -3,13 +3,9 @@ package com.sewjo.sewjo.Models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface FabricRepo extends JpaRepository<Fabric, Integer> {
-    List<Fabric> findByIdAndUser(int id, User user);
+public interface FabricRepo extends JpaRepository<Fabric, Integer>{
     List<Fabric> findAll();
-    List<Fabric> findAllByUser(User user);
-
     List<Fabric> findByName(String name);
-    List<Fabric> findByProjectId(int projectId);
     Fabric findById(int id);
     void deleteById(int id);
 }
