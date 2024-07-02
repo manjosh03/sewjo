31c5e9e2ac24872740eb0457e6e7fa7dd40b9802
Ali Naseri <ali@naseri.live>
2024-07-01 20:52:11 -0700

    Implement login and copy login page from bobby for testing purposes

--- src/main/java/com/sewjo/sewjo/Controllers/ProjectController.java
+++ src/main/java/com/sewjo/sewjo/Controllers/ProjectController.java
@@ -1,4 +1,16 @@
 package com.sewjo.sewjo.Controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import com.sewjo.sewjo.Models.Fabric;
+import com.sewjo.sewjo.Models.FabricRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class ProjectController {
 }
