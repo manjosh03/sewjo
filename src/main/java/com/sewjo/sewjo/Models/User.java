3ea54fdb993c989bfd5490069e0ac33e0b9ca1f2
Ali Naseri <ali@naseri.live>
2024-07-01 20:30:28 -0700

    Link User class to subclasses

--- src/main/java/com/sewjo/sewjo/Models/User.java
+++ src/main/java/com/sewjo/sewjo/Models/User.java
@@ -2,6 +2,7 @@ package com.sewjo.sewjo.Models;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "Users")
@@ -17,12 +18,52 @@ public class User {
     private List<Fabric> fabrics;
     @OneToMany
     private List<Pattern> patterns;
+
     public User() {
     }
 
     public User(String password, String email) {
         this.password = password;
         this.email = email;
+        this.fabrics = new ArrayList<>();
+        this.patterns = new ArrayList<>();
+        this.projects = new ArrayList<>();
+    }
+
+    public void addFabric(Fabric fabric) {
+        this.fabrics.add(fabric);
+    }
+
+    public void addPattern(Pattern pattern) {
+        this.patterns.add(pattern);
+    }
+
+    public void addProject(Project project) {
+        this.projects.add(project);
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public List<Fabric> getFabrics() {
+        return fabrics;
+    }
+
+    public List<Pattern> getPatterns() {
+        return patterns;
+    }
+
+    public void removeFabric(Fabric fabric) {
+        this.fabrics.remove(fabric);
+    }
+
+    public void removePattern(Pattern pattern) {
+        this.patterns.remove(pattern);
+    }
+
+    public void removeProject(Project project) {
+        this.projects.remove(project);
     }
 
     public int getId() {
