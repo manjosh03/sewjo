c18c54b67fc840de91b694bf0cbb3707acce823a
Ali Naseri <ali@naseri.live>
2024-07-01 19:59:31 -0700

    Create fabric creation method

--- src/test/java/com/sewjo/sewjo/SewjoApplicationTests.java
+++ src/test/java/com/sewjo/sewjo/SewjoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.sewjo.sewjo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
