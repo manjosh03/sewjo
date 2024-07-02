f2ff985f550a67d34e074f8d17a8dadbd1792e1b
Ali Naseri <ali@naseri.live>
2024-07-01 20:20:08 -0700

    Create the User class and methods

--- src/main/java/com/sewjo/sewjo/Models/User.java
+++ src/main/java/com/sewjo/sewjo/Models/User.java
@@ -2,8 +2,6 @@ package com.sewjo.sewjo.Models;
 import jakarta.persistence.*;
 
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Properties;
 
 @Entity
 @Table(name = "Users")
@@ -11,70 +9,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
     private String email;
     private String password;
     @OneToMany
     private List<Project> projects;
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToMany
     private List<Fabric> fabrics;
     @OneToMany
     private List<Pattern> patterns;
-
     public User() {
     }
 
-    public User(String name, String password, String email) {
+    public User(String password, String email) {
         this.password = password;
         this.email = email;
-        this.name = name;
-        this.fabrics = new ArrayList<>();
-        this.patterns = new ArrayList<>();
-        this.projects = new ArrayList<>();
-    }
-
-    public void addFabric(Fabric fabric) {
-        this.fabrics.add(fabric);
-    }
-
-    public void addPattern(Pattern pattern) {
-        this.patterns.add(pattern);
-    }
-
-    public void addProject(Project project) {
-        this.projects.add(project);
-    }
-
-    public List<Project> getProjects() {
-        return projects;
-    }
-
-    public List<Fabric> getFabrics() {
-        return fabrics;
-    }
-
-    public List<Pattern> getPatterns() {
-        return patterns;
-    }
-
-    public void removeFabric(Fabric fabric) {
-        this.fabrics.remove(fabric);
-    }
-
-    public void removePattern(Pattern pattern) {
-        this.patterns.remove(pattern);
-    }
-
-    public void removeProject(Project project) {
-        this.projects.remove(project);
     }
 
     public int getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setId(int id) {
--- src/main/java/com/sewjo/sewjo/Models/UserRepo.java
+++ src/main/java/com/sewjo/sewjo/Models/UserRepo.java
@@ -1,9 +1,5 @@
 package com.sewjo.sewjo.Models;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepo {
 
-public interface UserRepo extends JpaRepository<User,Integer> {
-    List<User> findByEmailAndPassword(String email, String password);
-    User findById(int userId);
 }
