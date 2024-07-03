d2fd3276ba91ed3201623c8171e475c260d07263
manjosh03 <manjosh01@gmail.com>
2024-07-03 00:54:46 -0700

    path addition

--- src/main/java/com/sewjo/sewjo/controllers/UserController.java
+++ src/main/java/com/sewjo/sewjo/controllers/UserController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@RequestMapping("/api/users")
 @CrossOrigin
 public class UserController {
 
