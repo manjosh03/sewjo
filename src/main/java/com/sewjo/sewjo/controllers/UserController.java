86eb83140671717b10950fb45d56eaf79f0726cc
manjosh03 <manjosh01@gmail.com>
2024-07-02 19:18:39 -0700

    add neccessary imports

--- src/main/java/com/sewjo/sewjo/controllers/UserController.java
+++ src/main/java/com/sewjo/sewjo/controllers/UserController.java
@@ -1,5 +1,26 @@
 package com.sewjo.sewjo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sewjo.sewjo.models.User;
+import com.sewjo.sewjo.models.UserRepository;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@CrossOrigin
 public class UserController {
 
 }
