9a690158a80f4ff43a74694cf69b99f518659117
Tushar Bector <bectort14@gmail.com>
2024-07-03 12:44:35 -0700

    Fixed models

--- src/main/java/com/sewjo/sewjo/controllers/UserController.java
+++ src/main/java/com/sewjo/sewjo/controllers/UserController.java
@@ -25,9 +25,11 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
-@CrossOrigin
+@RequestMapping("/login")
+
 public class UserController {
 
     @Autowired
