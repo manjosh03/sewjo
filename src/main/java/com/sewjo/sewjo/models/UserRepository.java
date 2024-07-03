ca336ad120640851a24b300c8f07d77e92e3a1fb
manjosh03 <manjosh01@gmail.com>
2024-07-02 19:10:48 -0700

    added UserRepository in models

---
+++ src/main/java/com/sewjo/sewjo/models/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sewjo.sewjo.models;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findBySize(int size);
+
+    List<User> findByNameAndPassword(String name, String Password);
+}
