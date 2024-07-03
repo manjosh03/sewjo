3c23ff382a2ddf6d05844f8eeac6414bfe4ecd95
Tushar Bector <bectort14@gmail.com>
2024-07-02 20:34:02 -0700

    added intial react app for login

---
+++ loginApp/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+
+  const handleToggle = () => {
+    setIsSignUp(!isSignUp);
+  };
+
+  return (
+    <div className={`container ${isSignUp ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
+        <form>
+          <h1 className="basic">Create Account</h1>
+          <span className="basic"> use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Phone Number" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="button">Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in">
+        <form>
+          <h1 className="basic">Sign In</h1>
+
+          <span className="basic">use your email and password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forget Your Password?</a>
+          <button type="button">Sign In</button>
+        </form>
+      </div>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of site features</p>
+            <button className="hidden" id="login" onClick={handleToggle}>
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>
+              Register with your personal details to use all of site features
+            </p>
+            <button className="hidden" id="register" onClick={handleToggle}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
