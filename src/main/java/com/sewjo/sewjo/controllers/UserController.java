2e2d1b306acd0b7fcfdbeddbb2422ef1bec23aa1
manjosh03 <manjosh01@gmail.com>
2024-07-02 21:38:52 -0700

    added comments to work further

--- src/main/java/com/sewjo/sewjo/controllers/UserController.java
+++ src/main/java/com/sewjo/sewjo/controllers/UserController.java
@@ -49,31 +49,35 @@ public class UserController {
         return "users/addedUser";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/login") // This is getmapping when we enter in the url to go to login page
     public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
         User user = (User) session.getAttribute("session_user");
         if (user == null) {
-            return "users/login";
+            return "users/login"; // this is the case when user has not logged in, show the page for log in
         } else {
             model.addAttribute("user", user);
-            return "users/protected";
-        }
+            return "users/protected"; // this is accessible for users who are logged in, need to change the location
+                                      // as we don't have the folder users under templates and no login.html we also
+                                      // don't have the protected.html file which will show the features that the
+                                      // registered users have we can redirect the user to the fabric page
+        } // Above will either take me to the page of login/signUp or will take me to the
+          // page where user can do smthn when logged in
     }
 
     @PostMapping("/login")
     public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request,
             HttpSession session) {
-        // processing login
+        // processing login, when user hits the sign in button
         String name = formData.get("name");
         String pwd = formData.get("password");
         List<User> userlist = userRepository.findByNameAndPassword(name, pwd);
         if (userlist.isEmpty()) {
-            return "users/login";
+            return "users/login"; // re direct back to login if they enter the wrong password or username
         } else {
             // success
-            User user = userlist.get(0);
+            User user = userlist.get(0); // getting which user it is who logged in
             request.getSession().setAttribute("session_user", user);
-            model.addAttribute("user", user);
+            model.addAttribute("user", user); // user logged in
             return "users/protected";
         }
     }
@@ -81,6 +85,6 @@ public class UserController {
     @GetMapping("/logout")
     public String destroySession(HttpServletRequest request) {
         request.getSession().invalidate();
-        return "/users/login";
+        return "/users/login"; // Go back to the initial login page
     }
 }
--- src/main/java/com/sewjo/sewjo/models/UserRepository.java
+++ src/main/java/com/sewjo/sewjo/models/UserRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findBySize(int size);
 
-    List<User> findByNameAndPassword(String name, String Password);
+    List<User> findByNameAndPassword(String name, String Password); // this gives a list of users by name and password
+    // if the name and password matches in the list then that means we have that
+    // user as a registered member already, if it gives us null then the user needs
+    // to sign up first
 }
