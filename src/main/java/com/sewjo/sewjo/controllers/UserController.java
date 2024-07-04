a5dab51bb6a3995c3dfac4f3751525a8f58ac14d
manjosh03 <manjosh01@gmail.com>
2024-07-04 00:49:55 -0700

    Adding onto controller1

--- src/main/java/com/sewjo/sewjo/controllers/UserController.java
+++ src/main/java/com/sewjo/sewjo/controllers/UserController.java
@@ -1,30 +1,14 @@
 package com.sewjo.sewjo.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.sewjo.sewjo.models.UserRepository;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sewjo.sewjo.Dto.UserDTO;
+import com.sewjo.sewjo.models.UserRepository;
 
 @RestController
 @CrossOrigin
@@ -40,8 +24,10 @@ public class UserController {
     // }
 
     @PostMapping(path = "/save") // This is when user signs up in the form
-    public String saveUser(@RequestBody UserDTO UserDTO) {
+    public String saveUser(@RequestBody UserDTO userDTO) {
 
+        String id = userService.addUser(userDTO);
+        return id;
     }
 
 }
