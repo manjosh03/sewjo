0f397b1ee13f7552859ec5ac7c702caf69a6b8ac
manjosh03 <manjosh01@gmail.com>
2024-07-04 01:05:42 -0700

    changes to user repo

---
+++ src/main/java/com/sewjo/sewjo/Repo/UserRepository.java
@@ -0,0 +1,23 @@
+package com.sewjo.sewjo.Repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import com.sewjo.sewjo.Entity.User;
+
+@EnableJpaRepositories
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findOneEmailAndPassword(String email, String Password); // this gives a list of users by name and
+                                                                           // password
+    // if the name and password matches then that means we have that
+    // user as a registered member already, if it gives us null then the user needs
+    // to sign up first
+
+    User findByEmail(String email);
+}
---
+++ src/main/java/com/sewjo/sewjo/Service/impl/UserIMPL.java
@@ -0,0 +1,14 @@
+package com.sewjo.sewjo.Service.impl;
+
+import com.sewjo.sewjo.Dto.UserDTO;
+import com.sewjo.sewjo.Service.UserService;
+
+public class UserIMPL implements UserService {
+
+    @Override
+    public String addUser(UserDTO userDTO) {
+
+        return null;
+    }
+
+}
