f95c413301c9cc40bf81dee5197346120f27e52d
Ali Naseri <ali@naseri.live>
2024-07-16 16:15:10 -0700

    Linked Front End

--- src/main/java/com/sewjo/sewjo/Controllers/PageController.java
+++ src/main/java/com/sewjo/sewjo/Controllers/PageController.java
@@ -16,7 +16,7 @@ public class PageController {
 
     @GetMapping("/")
     public String index(Model model) {
-        return "redirect:/login";
+        return "redirect:/sewjohome.html";
     }
 
 
