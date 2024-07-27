e774b957be28e6e429f3b3310b71f8a86f77e9b6
Ali Naseri <ali@naseri.live>
2024-07-26 20:53:32 -0700

    Added functionality to upload pictures using firebase. fixed user page

--- src/main/java/com/sewjo/sewjo/Models/User.java
+++ src/main/java/com/sewjo/sewjo/Models/User.java
@@ -27,10 +27,11 @@ public class User {
     public User() {
     }
 
-    public User(String name, String password, String email) {
+    public User(String name, String password, String email, String ProfilePicture) {
         this.password = password;
         this.email = email;
         this.name = name;
+        this.ProfilePicture = ProfilePicture;
         this.bio = "Add Bio";
         this.fabrics = new ArrayList<>();
         this.patterns = new ArrayList<>();
